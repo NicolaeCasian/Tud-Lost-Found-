44307230a8389656a0d6b381d1dff33f0ce0231f
Akos Bujdoso <b00153898@mytudublin.ie>
2024-11-26 17:40:59 +0000

    lost-items-front-end

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,52 +1,94 @@
-import {IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar} from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+    IonCol,
+    IonContent,
+    IonGrid,
+    IonHeader, IonItem,
+    IonLabel, IonList,
+    IonMenu, IonMenuToggle,
+    IonPage,
+    IonRow,
+    IonSplitPane,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
 import React from 'react';
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+import {IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle} from '@ionic/react';
 
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Lost items</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Lost items</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-          <IonGrid>
-              <IonRow>
-                  {Array.from({ length: 4 }).map((_, index) => (
-                      <IonCol
-                          size="12" // Full-width on small screens
-                          sizeMd="6" // Half-width on medium+ screens
-                          key={index}
-                      >
-                          <IonCard>
-                              <img
-                                  alt="Silhouette of mountains"
-                                  src="https://ionicframework.com/docs/img/demos/card-media.png"
-                              />
-                              <IonCardHeader>
-                                  <IonCardTitle>Card Title {index + 1}</IonCardTitle>
-                                  <IonCardSubtitle>Card Subtitle {index + 1}</IonCardSubtitle>
-                              </IonCardHeader>
-                              <IonCardContent>
-                                  I'm baby knausgaard brunch jianbing, butcher literally food truck palo santo gastropub irony lomo ugh shaman af roof party dolore.
-                              </IonCardContent>
-                          </IonCard>
-                      </IonCol>
-                  ))}
-              </IonRow>
-          </IonGrid>
-      </IonContent>
-    </IonPage>
-  );
+    return (
+        <IonPage>
+            <IonSplitPane contentId="main-content">
+                {/* Side Navigation (Filters) */}
+                <IonMenu contentId="main-content" type="overlay">
+                    <IonHeader>
+                        <IonToolbar>
+                            <IonTitle>Filters</IonTitle>
+                        </IonToolbar>
+                    </IonHeader>
+                    <IonContent>
+                        <IonList>
+                            <IonMenuToggle auto-hide="false">
+                                <IonItem button>
+                                    <IonLabel>Category 1</IonLabel>
+                                </IonItem>
+                                <IonItem button>
+                                    <IonLabel>Category 2</IonLabel>
+                                </IonItem>
+                                <IonItem button>
+                                    <IonLabel>Category 3</IonLabel>
+                                </IonItem>
+                                <IonItem button>
+                                    <IonLabel>Category 4</IonLabel>
+                                </IonItem>
+                            </IonMenuToggle>
+                        </IonList>
+                    </IonContent>
+                </IonMenu>
+
+                {/* Main Content */}
+                <IonPage id="main-content">
+                    {/* Top Navigation */}
+                    <IonHeader>
+                        <IonToolbar>
+                            <IonTitle>Lost Items</IonTitle>
+                        </IonToolbar>
+                    </IonHeader>
+
+                    <IonContent fullscreen>
+                        {/* Card Grid */}
+                        <IonGrid>
+                            <IonRow>
+                                {Array.from({ length: 6 }).map((_, index) => (
+                                    <IonCol
+                                        size="12"
+                                        sizeMd="6"
+                                        sizeLg="4"
+                                        key={index}
+                                    >
+                                        <IonCard>
+                                            <img
+                                                alt="Silhouette of mountains"
+                                                src="https://ionicframework.com/docs/img/demos/card-media.png"
+                                            />
+                                            <IonCardHeader>
+                                                <IonCardTitle>Card Title {index + 1}</IonCardTitle>
+                                                <IonCardSubtitle>Card Subtitle {index + 1}</IonCardSubtitle>
+                                            </IonCardHeader>
+                                            <IonCardContent>
+                                                I'm baby knausgaard brunch jianbing, butcher literally food truck palo santo gastropub irony lomo ugh shaman af roof party dolore.
+                                            </IonCardContent>
+                                        </IonCard>
+                                    </IonCol>
+                                ))}
+                            </IonRow>
+                        </IonGrid>
+                    </IonContent>
+                </IonPage>
+            </IonSplitPane>
+        </IonPage>
+    );
 };
 
 export default Tab1;
